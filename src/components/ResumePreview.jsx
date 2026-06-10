d98acc8d85720f9387e2169ea66ed322e755491c
4shm1td06 <alpha5sector@gmail.com>
2026-06-10 12:55:32 +0530

    fixed preview

--- src/components/ResumePreview.jsx
+++ src/components/ResumePreview.jsx
@@ -5,7 +5,7 @@ import { buildResumeHtml } from '../utils/mleTemplate.js';
 export function ResumePreview({ data = {} }) {
   const resume = normalizeResume(data);
 
-  const html = useMemo(() => buildResumeHtml(resume), [resume]);
+  const html = useMemo(() => buildResumeHtml(resume).replace(/__WATERMARK__/g, ''), [resume]);
 
   return (
     <div className="preview-sheet">
